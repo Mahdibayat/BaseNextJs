83957a864c3411ca83a3431528f9ae84ed0bc96f
mahdibayat <mahdibayat72@hotmail.com>
2021-04-26 15:39:50 +0430

    BaseNextJs with Redux

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1966cb834bbb880aa9f4";
+/******/ 	var hotCurrentHash = "cc021ad733a9adfe8a91";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
